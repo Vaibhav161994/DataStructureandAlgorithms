bb68eb343167296fbb9182b3d007126c0b7a2f85
Vaibhav Kaushik <vaibhavkaushik161994@gmail.com>
2019-08-13 16:46:07 +0530

    BubbleSortImpl

---
+++ BubbleSort.java
@@ -0,0 +1,39 @@
+public class BubbleSort {
+	
+	public static int[] sortArray(int [] input) {
+		
+		int swapped=0,temp=0;
+		
+		for(int i=0; i<input.length; i++) {
+			
+			if(i+1>=input.length) {
+				// prevent the loop to cross Upper Boundary[Array IndexOutofBound] 
+				i=0;
+				swapped++;
+			}			
+			
+			if(input[i]>input[i+1]) { // Check to swap the adjacent variables
+				temp=input[i];
+				input[i]=input[i+1];
+				input[i+1]=temp;
+			}
+			if(swapped>(input.length/2)+1) {
+				break;
+			}
+		}
+		
+		return input;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {3, 6, 1, 2, 9, 8, 1, 8};
+		
+		int [] result = BubbleSort.sortArray(arr);
+		
+		for (int i : result) {
+			System.out.print(i+"\t");
+		}
+	}
+
+}
